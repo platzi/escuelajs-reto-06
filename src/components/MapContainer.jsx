fd8ad63c68cf1a85a3a7bab3493bb3dd510b0f3c
Dumar Ojeda <dumarojeda@gmail.com>
2019-10-05 12:02:00 -0500

    Resolviendo el segundo problema

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -17,12 +17,15 @@ export class MapContainer extends Component {
           google={this.props.google}
           style={{width: '100%', height: '400px', position: 'relative'}}
           className={'map'}
-          zoom={5}
+          zoom={4}
           initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
           visible={this.state.show}
         >
           <Marker
             position={{ lat: 19.4267261, lng: -99.1718706 }}
+          />
+          <Marker
+            position={{ lat: 4.6560716, lng: -74.0595918 }}
           />
         </Map>
         <MapButton toggleMap={this.toggleMap} isMapVisible={this.state.show} />
