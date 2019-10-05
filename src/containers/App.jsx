39b51a96af481049fd575feeca70c7d953907f23
Erik Sánchez <erik.sape@gmail.com>
2019-10-05 11:07:19 -0500

    resuelto todos los retos :)

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MapContainer from "../components/MapContainer";
 import '../styles/containers/App.styl';
 
 const App = () => {
+  const [show, changeShow] = useState(false);
   return (
     <div className="App">
-      <MapContainer />
+      <button onClick={() => changeShow(!show)}>
+        {show ? 'Ocultar' : 'Mostrar'} Mapa
+      </button>
+      {show && <MapContainer /> }
     </div>
   )
 };
