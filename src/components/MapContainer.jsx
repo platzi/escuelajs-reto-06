b7122a0fc865c1728c14d10206bc497d6ac31827
Jeff Barragan <leshz@me.com>
2019-10-05 10:45:21 -0500

    Primer reto

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,19 +1,40 @@
 import React from 'react';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
-const MapContainer = ({ google }) => {
-  return (
-    <Map
-      google={google}
-      zoom={5}
-      initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
-    >
-      <Marker
-        position={{ lat: 19.4267261, lng: -99.1718706 }}
-      />
-    </Map>
-  );
-}
+
+class MapContainer extends React.Component {
+
+  constructor(props) { 
+    super(props);
+    this.state = {show:true}
+  }
+
+  handleClick () { 
+    const { show } = this.state;
+    this.setState({show:!show})
+  }
+
+
+  render() { 
+
+    const { google } = this.props;
+    const { show } = this.state;
+
+    return (
+      <Map
+        google={google}
+        zoom={5}
+        visible={show}
+        initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
+      >
+        <Marker
+          position={{ lat: 19.4267261, lng: -99.1718706 }}
+        />
+        <button className="button" type="button" onClick={this.handleClick.bind(this)}> show Maps   </button>
+      </Map>
+    );
+  }
+ }
 
 export default GoogleApiWrapper({
   apiKey: 'AIzaSyCmjvkXB_DMnBUNwxQztLMStyQmA_szbNw'
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -6,6 +6,7 @@ const App = () => {
   return (
     <div className="App">
       <MapContainer />
+      
     </div>
   )
 };
