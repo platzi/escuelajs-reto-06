48cff4ebe184f08fe33a014a3ff5ce24e1bcc048
Kristian Lopez <kristian.lom@gmail.com>
2019-10-05 12:01:32 -0500

    Solución Tercer Problema.

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,26 +1,30 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import { GoogleApiWrapper, Map, Marker } from 'google-maps-react';
 
-class MapContainer extends Component  {
+class MapContainer extends Component {
 
-  render(){
+  render() {
+
     return (
       <Map
         google={this.props.google}
         zoom={5}
-        initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
-      >
-        <Marker
-          position={{ lat: 19.4267261, lng: -99.1718706 }}
-        />
-        <Marker
-          position={{ lat: 4.6560716, lng: -74.0595918}}
-        />
+        initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}>
+        {
+          this.props.locations.map(item =>
+            (
+              <Marker
+                key={item.venueName}
+                position={{ lat: item.venueLat, lng: item.venueLon }}
+              />
+            ),
+          )
+        }
       </Map>
     );
   }
 }
 
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyCmjvkXB_DMnBUNwxQztLMStyQmA_szbNw'
+  apiKey: 'AIzaSyCmjvkXB_DMnBUNwxQztLMStyQmA_szbNw',
 })(MapContainer);
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,21 +1,28 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import MapContainer from '../components/MapContainer';
 import '../styles/containers/App.styl';
 
 const App = () => {
-  const [showMap, setShowMap] = useState(false);
+  const API = 'http://localhost:3000/locations';
+  const [showMap, setShowMap] = useState(true);
+  const [locations, setLocations] = useState([]);
+
+  useEffect(() => {
+     fetch(API)
+      .then(response => response.json())
+      .then(res => setLocations(res));
+  }, []);
 
   const show = () => {
     setShowMap(!showMap);
   };
-
 
   return (
     <div className="App">
       <button type="button" className="ButtonHandler" onClick={show}>{showMap ? 'Hide' : 'Show'}</button>
       {
         showMap &&
-          <MapContainer />
+        <MapContainer locations={locations} />
       }
     </div>
   );
