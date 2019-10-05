4f55f2dd96d479ac7b12af6dadcad2ff72f5f3c3
Solej77 <ing.josgalo@gmail.com>
2019-10-05 18:19:34 -0500

    second task done

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -31,11 +31,14 @@ class MapContainer extends Component {
         {visibility && (
           <Map
             google={google}
-            zoom={5}
+            zoom={4}
             initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
           >
             <Marker
-              position={{ lat: 19.4267261, lng: -99.1718706 }}
+              position={{ lat: 19.42672619, lng: -99.1718706 }}
+            />
+            <Marker
+              position={{ lat: 4.6560716, lng: -74.0595918 }}
             />
           </Map>
         )}
