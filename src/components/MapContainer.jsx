11d80665c2ea911bf77cf4196946968f93e29181
proartmateur <ennimandroid@gmail.com>
2019-10-05 11:09:10 -0500

    Transform MapContainer Component to Stateful

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
-const MapContainer = ({ google }) => {
+class MapContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      google: this.props.google
+    }
+  }
+  render () {
+    return (
+      <Map
+      google={google}
+      zoom={5}
+      initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
+    >
+      <Marker
+        position={{ lat: 19.4267261, lng: -99.1718706 }}
+      />
+    </Map>
+    );
+  }
+  
+};
+
+/* const MapContainer = ({ google }) => {
   return (
     <Map
       google={google}
@@ -13,7 +36,7 @@ const MapContainer = ({ google }) => {
       />
     </Map>
   );
-}
+} */
 
 export default GoogleApiWrapper({
   apiKey: 'AIzaSyCmjvkXB_DMnBUNwxQztLMStyQmA_szbNw'
