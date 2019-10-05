62336cb4ab666517bc0a72fd5b67b051729ce570
Jesús Antonio García Valadez <jesus.garciav@me.com>
2019-10-05 10:09:20 -0500

    feat: add additional marker for Platzi Bogotá

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -5,11 +5,14 @@ const MapContainer = ({ google }) => {
   return (
     <Map
       google={google}
-      zoom={5}
+      zoom={4}
       initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
     >
       <Marker
         position={{ lat: 19.4267261, lng: -99.1718706 }}
+      />
+      <Marker
+        position={{ lat: 4.6560716, lng: -74.0595918 }}
       />
     </Map>
   );
