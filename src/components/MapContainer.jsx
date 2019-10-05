6387d3898f53bc04548432f05f927f0c5e6b9ee3
Solej77 <ing.josgalo@gmail.com>
2019-10-05 17:59:23 -0500

    first task done

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,18 +1,48 @@
-import React from 'react';
+/* eslint-disable no-undef */
+import React, { Component } from 'react';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
-const MapContainer = ({ google }) => {
-  return (
-    <Map
-      google={google}
-      zoom={5}
-      initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
-    >
-      <Marker
-        position={{ lat: 19.4267261, lng: -99.1718706 }}
-      />
-    </Map>
-  );
+class MapContainer extends Component {
+  state = { 
+    // eslint-disable-next-line react/no-unused-state
+    google,
+    visibility: false,
+  }
+
+  toggleMap = () => {
+    const {visibility} = this.state;
+    let stateCurrent = false;
+    if (visibility) {
+      stateCurrent = false;
+    } else {
+      stateCurrent = true;
+    }
+      
+    this.setState({
+      visibility: stateCurrent,
+    });
+  }
+
+  render() {
+    const {visibility} = this.state;
+    return (
+      <>
+        <button type="button" onClick={this.toggleMap}>Mapa</button>
+        {visibility && (
+          <Map
+            google={google}
+            zoom={5}
+            initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
+          >
+            <Marker
+              position={{ lat: 19.4267261, lng: -99.1718706 }}
+            />
+          </Map>
+        )}
+      </>
+    );
+  }
+
 }
 
 export default GoogleApiWrapper({
