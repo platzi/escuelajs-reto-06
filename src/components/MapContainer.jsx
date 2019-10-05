d827b1fa3acba07aede175cae94cec180528b5f3
WIZANCHEZ_MACKBOOK <wizanchez@mackbook.com>
2019-10-05 10:50:40 -0500

    resuto

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,18 +1,72 @@
 import React from 'react';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
-const MapContainer = ({ google }) => {
-  return (
-    <Map
-      google={google}
-      zoom={5}
-      initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
-    >
-      <Marker
-        position={{ lat: 19.4267261, lng: -99.1718706 }}
-      />
-    </Map>
-  );
+class MapContainer extends React.Component{
+
+	constructor(props){
+		super(props);
+
+		this.state ={
+			showMap : true,
+			vDataFakeAPI :this.props.data
+		}
+
+		//console.log('0000isddisndnsknds===',this.props.data);
+	};
+
+	changeStatusMap =(e ) =>{
+
+		this.setState({
+			showMap: this.state.showMap?false:true
+		});
+	};
+
+	ShowButton(){
+
+		const title = this.state.showMap?'Ocultar ':'Mostar ';
+
+		return (<button type="button" style={{display:'inline'}} onClick={ this.changeStatusMap }>{`${title} Mapa`}</button>);
+	};
+
+	render(){
+
+		const vSHowMap = this.state.showMap?'inline':'none';
+		const test = this.props.test
+		return (
+			<React.Fragment>
+				{ this.ShowButton() }
+				<div
+					style={{display:vSHowMap}}
+				>{}
+					<Map
+						google={google}
+						zoom={4}
+						initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
+						style={{width: '80%', height: '80%', position: 'relative'}}
+					>{
+
+						this.state.vDataFakeAPI.map( (index)=>{
+
+							const lati = index.venueLat;
+							const long = index.venueLon;
+							const vTitle = index.venueName;
+
+							return (
+								<Marker
+							position={{ lat: lati, lng: long }}
+							title={ vTitle}
+						/>
+							)
+						})
+					}
+						
+
+					</Map>
+				</div>
+				
+			</React.Fragment>
+		);
+	}
 }
 
 export default GoogleApiWrapper({
