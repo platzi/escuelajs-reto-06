1b6312e83da810943891aef4f359bc25098f2f1d
Jesús Antonio García Valadez <jesus.garciav@me.com>
2019-10-05 09:57:44 -0500

    feat: add button for show/hide the map
    feat: change implementation using React Component

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import MapContainer from "../components/MapContainer";
 import '../styles/containers/App.styl';
 
-const App = () => {
-  return (
-    <div className="App">
-      <MapContainer />
-    </div>
-  )
+class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      show: false,
+    };
+    this.toggleMap = this.toggleMap.bind(this);
+  }
+
+  toggleMap () {
+    this.setState(state => ({
+      show: !state.show
+    }));
+  }
+
+  render () {
+    const show = this.state.show;
+
+    return (
+      <div className="App">
+        <button type="button" onClick={this.toggleMap}>
+          {show ? 'Mostrar' : 'Ocultar'} Mapa
+        </button>
+        {show ? (<p>No hay nada que mostrar</p>) : (<MapContainer />)}
+      </div>
+    )
+  }
 };
 
 export default App;
