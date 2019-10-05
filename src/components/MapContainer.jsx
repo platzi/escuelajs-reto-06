6727135116c1f61fa8d80d36507962bfec6f70d1
Julian Toro <jtoro@tecmovin.com>
2019-10-05 10:19:48 -0500

    reto 1, reto 2, reto 3

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,18 +1,41 @@
 import React from 'react';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
-const MapContainer = ({ google }) => {
-  return (
-    <Map
-      google={google}
-      zoom={5}
-      initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
-    >
-      <Marker
-        position={{ lat: 19.4267261, lng: -99.1718706 }}
-      />
-    </Map>
-  );
+
+class MapContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isShow: false,
+    };
+  }
+
+  handleClick = (e) => {
+    e.preventDefault();
+    this.setState(state => ({ isShow: !state.isShow }));
+  }
+
+  render() {
+    const {google, markers} = this.props;
+    const {isShow} = this.state;
+    return (
+      <>
+        <div className='div__button'>
+          <button type='button' onClick={this.handleClick}>Mostrar</button>  
+        </div>     
+        {isShow && (
+          <Map
+            google={google}
+            zoom={5}
+            initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
+          >
+            {markers.map((item, i) => <Marker key={i} position={{ lat: item.venueLat, lng: item.venueLon }} /> )}            
+          </Map>
+        )}
+        
+      </>
+    );
+  }
 }
 
 export default GoogleApiWrapper({
