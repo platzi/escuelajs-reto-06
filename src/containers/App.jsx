71a750e2d6035b3bc2f733cf899511cc83e6d160
unknown <johanrodriguez_14@hotmail.com>
2019-10-05 21:10:14 -0500

    Solucion full reto 06

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import MapContainer from "../components/MapContainer";
 import '../styles/containers/App.styl';
 
+const API = " http://localhost:3000/locations";
+
 const App = () => {
+
+  const [locations, setLocations] = useState([]);
+
+  useEffect(() => {
+     fetch(API)
+      .then(reponse => reponse.json())
+      .then(responseLocations => setLocations(responseLocations));
+  }, []);
+
   return (
     <div className="App">
-      <MapContainer />
+      <MapContainer locations={locations} />
     </div>
   )
 };
