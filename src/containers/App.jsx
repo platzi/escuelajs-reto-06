db7c905462b1fd707c95928c2da7175c417f66a3
mac <mac@MacBook-Pro-de-mac.local>
2019-10-06 13:21:45 -0500

    Reto 4

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,17 +1,24 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect} from 'react';
 import MapContainer from "../components/MapContainer";
 import '../styles/containers/App.styl';
 
 const App = () => {
   const [show, setShow] = useState(false);
+  const [locations, setLocations ] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/locations')
+      .then(response => response.json())
+      .then(data => setLocations(data));
+  },[]);
 
   return (
     <div className='main'>
-      <div>
-        <button type='button' onClick={() => setShow(!show)}>{show ? 'Hide Map':'Show Map'}</button>
+      <div className='aside'>
+        <button className='btn' type='button' onClick={() => setShow(!show)}>{show ? 'Hide Map':'Show Map'}</button>
       </div>
       <div>
-        <MapContainer show={show} />
+        <MapContainer show={show} locations={locations} />
       </div>
     </div>
   )
