3c3212f27922cd37e7ccc84e4778e8570d1539ea
AryRosvall <ary.rosvall@gmail.com>
2019-10-06 13:06:51 -0500

    Challenge 1 to 4 done

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,18 +1,54 @@
-import React from 'react';
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+/* eslint-disable react/prefer-stateless-function */
 
-const MapContainer = ({ google }) => {
-  return (
-    <Map
-      google={google}
-      zoom={5}
-      initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
-    >
-      <Marker
-        position={{ lat: 19.4267261, lng: -99.1718706 }}
-      />
-    </Map>
-  );
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
+class MapContainer extends Component {
+
+  state = {
+    selectedPlace: {},
+    activeMarker: {},
+    showingInfoWindow: false,
+  };
+
+  onMarkerClick = (props, marker) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+  });
+
+  render() {
+    const { activeMarker, showingInfoWindow, selectedPlace } = this.state;
+    const { locations, isShow, google } = this.props;
+    
+    return !isShow ? <h2>haz clic para habilitar el mapa</h2> : (
+      <Map
+        google={google}
+        zoom={5}
+        initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
+      >
+        {
+          locations.map((location) => (
+            <Marker
+              onClick={this.onMarkerClick}
+              position={{ lat: location.venueLat, lng: location.venueLon }}
+              name={location.venueName}
+              key={location.venueName}
+            />
+          )
+          )
+        }
+        <InfoWindow
+          marker={activeMarker}
+          visible={showingInfoWindow}
+        >
+          <div>
+            <h1>{selectedPlace.name}</h1>
+          </div>
+        </InfoWindow>
+      </Map>
+    );
+  }
 }
 
 export default GoogleApiWrapper({
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import MapContainer from "../components/MapContainer";
 import '../styles/containers/App.styl';
 
-const App = () => {
-  return (
-    <div className="App">
-      <MapContainer />
-    </div>
-  )
-};
+const API = 'http://localhost:3000/locations';
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      isShow: false,
+      locations: {}
+    }
+  }
+  
+  handleClick = () => {
+    const { isShow } = this.state;
+    
+    this.setState({ isShow: !isShow, });
+    console.log(isShow)
+  }
+  
+  componentDidMount =() =>{
+    const { locations } = this.state;
+    fetch(API)
+    .then((response) => response.json())
+    .then((data) => this.setState({locations: data}));
+  }
+
+  render(){
+    const { isShow } = this.state;
+    return (
+      <div className="App">
+        <button onClick={this.handleClick}>Haz clic</button>
+        <MapContainer isShow={isShow} locations={this.state.locations} />
+      </div>
+    )
+  }
+}
 
 export default App;
