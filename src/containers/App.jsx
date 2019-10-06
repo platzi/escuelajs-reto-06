1e66cf38f7b833d8c9f93a82e712233a427854a5
Jose Manuel Salazar <soltisam@gmail.com>
2019-10-05 23:31:50 -0500

    Reto 6 - Primero, Segundo y Tercer Problema

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import MapContainer from "../components/MapContainer";
 import '../styles/containers/App.styl';
 
+const API_LOCAL = 'http://localhost:3000/locations';
+
 const App = () => {
+  const [markers, serMarker] = useState([]);
+
+  useEffect(() => {
+    fetch(API_LOCAL)
+    .then(response => response.json())
+    .then(dataLocations => serMarker(dataLocations))
+  }, [])
+
   return (
     <div className="App">
-      <MapContainer />
+      <MapContainer markers={markers} />
     </div>
   )
 };
