534c891dbd9358a101155681cc082392027c1b61
Zahira <zahira.rosales@gmail.com>
2019-10-06 08:44:57 -0500

    Primer y segundo reto

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
-const MapContainer = ({ google }) => {
-  return (
-    <Map
-      google={google}
-      zoom={5}
-      initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
-    >
-      <Marker
-        position={{ lat: 19.4267261, lng: -99.1718706 }}
-      />
-    </Map>
-  );
+class MapContainer extends React.Component{
+  render(){
+    return (
+      <Map
+        google={google}
+        zoom={5}
+        initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
+      >
+        <Marker
+          position={{ lat: 19.4267261, lng: -99.1718706 }}
+        />
+        <Marker
+        position={{ lat: 4.6560716, lng: -74.0595918 }}
+        />
+      </Map>
+    );
+  }
 }
 
 export default GoogleApiWrapper({
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MapContainer from "../components/MapContainer";
 import '../styles/containers/App.styl';
 
 const App = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClick = () => {
+    setShow(!show);
+  };
+  
   return (
     <div className="App">
-      <MapContainer />
+      <button onClick={handleClick}>{show ? 'Hide' : 'Show'}</button>
+      {show && <MapContainer />}
     </div>
   )
 };
