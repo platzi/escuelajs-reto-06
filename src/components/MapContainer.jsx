a344eaad8dca2a35ce7b701258f9a2c4abecd28b
Rob <betoarpi@gmail.com>
2019-10-06 21:40:13 -0500

    Segundo Problema

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -35,7 +35,14 @@ class MapContainer extends Component {
 
         {show ? (
           <Map google={google} zoom={5} initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}>
-            <Marker position={{ lat: 19.4267261, lng: -99.1718706 }} />
+            <Marker
+              title='Platzi HQ México'
+              position={{ lat: 19.4267261, lng: -99.1718706 }}
+            />
+            <Marker
+              title='Platzi HQ Bogotá'
+              position={{ lat: 4.6560716, lng: -74.0595918 }}
+            />
           </Map>
         ) :
           (
