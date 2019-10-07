e5d4d33c17ecedc8ede001a8cab7500e92a07e1f
cesar galindo <csgalindos@hotmail.com>
2019-10-06 19:03:54 -0500

    primer y segundo punto

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,19 +1,32 @@
 import React from 'react';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
-const MapContainer = ({ google }) => {
-  return (
+// eslint-disable-next-line react/prefer-stateless-function
+class MapContainer extends React.Component{
+constructor () {
+  super();
+  this.state = [];
+}
+
+render () {
+  return(
     <Map
-      google={google}
+      // eslint-disable-next-line react/destructuring-assignment
+      google={this.props.google}
       zoom={5}
       initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
     >
       <Marker
         position={{ lat: 19.4267261, lng: -99.1718706 }}
       />
+      <Marker
+        position={{ lat: 4.6560716, lng: -74.0595918 }}
+      />
     </Map>
-  );
+  )
+};
 }
+
 
 export default GoogleApiWrapper({
   apiKey: 'AIzaSyCmjvkXB_DMnBUNwxQztLMStyQmA_szbNw'
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+/* eslint-disable react/button-has-type */
+import React, {useState} from 'react';
 import MapContainer from "../components/MapContainer";
 import '../styles/containers/App.styl';
 
 const App = () => {
+  const [show,setShow] =  useState(true)
+  const handleClick = () => {setShow(!show)}
+
   return (
-    <div className="App">
-      <MapContainer />
+    <div className='App'>
+      <button onClick={handleClick}>{show ? 'Ocultar Mapa' : 'Mostrar Mapa'}</button>
+      {show && <MapContainer />}
     </div>
   )
 };
