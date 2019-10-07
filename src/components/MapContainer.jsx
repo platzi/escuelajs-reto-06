1485ee9397bf5fc0389bc5e6a79914af05aa44a6
Alvaro Baena <aabaenar@gmail.com>
2019-10-07 17:28:12 -0500

    Ver README.md, solo falta el fetch y he completado el reto

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,20 +1,61 @@
-import React from 'react';
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import React, { useState } from 'react';
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
 
-const MapContainer = ({ google }) => {
+const MapContainer = props => {
+  const { google, locations } = props;
+  const [activeMaker, setActiveMaker] = useState({
+    selectedPlace: null,
+    activeMarker: null,
+    showingInfoWindow: true,
+  });
+  const [showMapStatus, setShowMapStatus] = useState(true);
+  const onMarkerClick = (propsMaker, marker) => {
+    setActiveMaker({
+      selectedPlace: propsMaker,
+      activeMarker: marker,
+      showingInfoWindow: true,
+    });
+  };
   return (
-    <Map
-      google={google}
-      zoom={5}
-      initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
-    >
-      <Marker
-        position={{ lat: 19.4267261, lng: -99.1718706 }}
-      />
-    </Map>
+    <div>
+      <Map
+        google={google}
+        zoom={5}
+        visible={showMapStatus}
+        initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
+      >
+        {locations.map(location => (
+          <Marker
+            key={location.venueLat + location.venueLon}
+            position={{ lat: location.venueLat, lng: location.venueLon }}
+            onClick={(propsMaker, marker) => onMarkerClick(propsMaker, marker)}
+            name={location.venueName}
+          />
+        ))}
+        <InfoWindow
+          marker={activeMaker.activeMarker}
+          visible={activeMaker.showingInfoWindow}
+        >
+          <div>
+            <h1>
+              {activeMaker.selectedPlace && activeMaker.selectedPlace.name}
+            </h1>
+          </div>
+        </InfoWindow>
+      </Map>
+      <button
+        type="button"
+        onClick={() => {
+          setShowMapStatus(!showMapStatus);
+        }}
+        className="button-hide-show"
+      >
+        {showMapStatus ? 'Hide' : 'Show'}
+      </button>
+    </div>
   );
-}
+};
 
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyCmjvkXB_DMnBUNwxQztLMStyQmA_szbNw'
+  apiKey: 'AIzaSyCmjvkXB_DMnBUNwxQztLMStyQmA_szbNw',
 })(MapContainer);
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,13 +1,33 @@
-import React from 'react';
-import MapContainer from "../components/MapContainer";
+import React, { useEffect, useState } from 'react';
+import MapContainer from '../components/MapContainer';
 import '../styles/containers/App.styl';
 
 const App = () => {
+  // const locations = [
+  //   {
+  //     venueLat: 19.42672619,
+  //     venueLon: -99.1718706,
+  //     venueName: 'Platzi HQ CDMX',
+  //   },
+  //   {
+  //     venueLat: 4.6560716,
+  //     venueLon: -74.0595918,
+  //     venueName: 'Platzi HQ Bogota',
+  //   },
+  // ];
+  const [locations, setLocations] = useState([]);
+  const getLocation = async () => {
+    const response = await fetch(`http://localhost:3000/locations`);
+    setLocations(response.json());
+  };
+  useEffect(() => {
+    getLocation();
+  });
   return (
     <div className="App">
-      <MapContainer />
+      <MapContainer locations={locations} />
     </div>
-  )
+  );
 };
 
 export default App;
