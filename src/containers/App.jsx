03017fe1e31fa6f7571d5bbc93668c8a0af4814e
saforus <1405pablo@gmail.com>
2019-10-06 22:06:13 -0500

    RETO 6

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
-import MapContainer from "../components/MapContainer";
+import React, { useState, useEffect } from 'react';
+import MapContainer from '../components/MapContainer';
 import '../styles/containers/App.styl';
 
 const App = () => {
+  const [show, setShow] = useState(false);
+  const [coordenada, setCoordenada] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/locations')
+      .then(response => response.json())
+      .then(data => setCoordenada(data));
+  }, []);
   return (
-    <div className="App">
-      <MapContainer />
+    <div className="main">
+      <div className="aside">
+        <button className="btn" type="button" onClick={() => setShow(!show)}>
+          {show ? 'Ocultar' : 'Mostrar'}
+        </button>
+      </div>
+      <div>
+        <MapContainer show={show} coordenada={coordenada} />
+      </div>
     </div>
-  )
+  );
 };
 
 export default App;
