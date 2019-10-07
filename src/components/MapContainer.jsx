dce80abca4155ef69bd192d30140cad75a1c6aeb
Ruber Hernandez <ruberhenandez@gmail.com>
2019-10-06 22:39:18 -0500

    se hace problema 1 y problema 2

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,20 +1,23 @@
-import React from 'react';
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
-const MapContainer = ({ google }) => {
-  return (
-    <Map
-      google={google}
-      zoom={5}
-      initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
-    >
-      <Marker
-        position={{ lat: 19.4267261, lng: -99.1718706 }}
-      />
-    </Map>
-  );
+class MapContainer extends Component {
+  render() {
+    const { google } = this.props;
+    return (
+      <Map
+        google={google}
+        zoom={4}
+        initialCenter={{ lat: 4.6560716, lng: -74.0595918 }}
+      >
+        <Marker position={{ lat: 19.4267261, lng: -99.1718706 }} />
+        <Marker position={{ lat: 4.6560716, lng: -74.0595918 }} />
+      </Map>
+    );
+  }
 }
 
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyCmjvkXB_DMnBUNwxQztLMStyQmA_szbNw'
+  apiKey: 'AIzaSyCmjvkXB_DMnBUNwxQztLMStyQmA_szbNw',
 })(MapContainer);
