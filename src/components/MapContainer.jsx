1d2af35fef2cdf36e6bfcd2c57b071ba7af5b4c3
luisrdz5 <luis@luisrodriguez.com>
2019-10-06 21:42:40 -0500

    Solved the first Problem

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,18 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
-const MapContainer = ({ google }) => {
-  return (
-    <Map
-      google={google}
-      zoom={5}
-      initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
-    >
-      <Marker
-        position={{ lat: 19.4267261, lng: -99.1718706 }}
-      />
-    </Map>
-  );
+
+class MapContainer extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      zoom: 5,
+      lat: 19.5943885,
+      lng: -97.9526044,
+      show: false
+    };
+    this.toggleShow = this.toggleShow.bind(this);
+  }
+
+  toggleShow() { 
+    if(this.state.show){
+      this.setState({
+        show: false
+      });
+    }
+    else{ 
+      this.setState({
+        show: true  
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <div>
+          <button 
+            className='Button' 
+            type='button' 
+            onClick={this.toggleShow}
+          > 
+                  Mostrar Mapa 
+          </button>
+        </div>
+        <div>
+          <Map
+            google={google}
+            zoom={this.state.zoom}
+            visible={this.state.show}
+            initialCenter={{ lat: this.state.lat, lng: this.state.lng }}
+          >
+            <Marker
+            position={{ lat: 19.4267261, lng: -99.1718706 }}
+          />
+          </Map>
+        </div>
+      </div>
+
+
+    );
+  }
+
 }
 
 export default GoogleApiWrapper({
