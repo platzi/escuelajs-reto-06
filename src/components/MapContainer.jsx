b3cfe261c8bc49a09cff7f937671234668913998
Rene Ismael <Ismael_br7@hotmail.com>
2019-10-07 02:19:34 -0500

    Reto06 Terminado Ismael

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,20 +1,67 @@
-import React from 'react';
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
 
-const MapContainer = ({ google }) => {
-  return (
-    <Map
-      google={google}
-      zoom={5}
-      initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
-    >
-      <Marker
-        position={{ lat: 19.4267261, lng: -99.1718706 }}
-      />
-    </Map>
-  );
+class MapContainer extends Component {
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {},
+  };
+
+  onMapClicked = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null,
+      });
+    }
+  };
+
+  onMarkerClick = (props, marker) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true,
+    });
+
+  render() {
+    const MapState = this.state;
+    const { estado } = this.props;
+
+    return estado.show ? (
+      <div className="Mapa__main--toogle-none">
+        <Map
+          google={google}
+          onClick={this.onMapClicked}
+          zoom={5}
+          initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
+        >
+          {estado.locations.map((positionPlatzi, key) => (
+            <Marker
+              name={positionPlatzi.venueName}
+              onClick={this.onMarkerClick}
+              position={{
+                lat: positionPlatzi.venueLat,
+                lng: positionPlatzi.venueLon,
+              }}
+              key={key}
+            />
+          ))}
+
+          <InfoWindow
+            marker={MapState.activeMarker}
+            visible={MapState.showingInfoWindow}
+          >
+            <div>
+              <h1>{MapState.selectedPlace.name}</h1>
+            </div>
+          </InfoWindow>
+        </Map>
+      </div>
+    ) : null;
+  }
 }
 
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyCmjvkXB_DMnBUNwxQztLMStyQmA_szbNw'
+  apiKey: 'AIzaSyCmjvkXB_DMnBUNwxQztLMStyQmA_szbNw',
 })(MapContainer);
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,13 +1,69 @@
 import React from 'react';
-import MapContainer from "../components/MapContainer";
-import '../styles/containers/App.styl';
+import MapContainer from '../components/MapContainer';
+import '../styles/containers/App.css';
+import axios from 'axios';
 
-const App = () => {
-  return (
-    <div className="App">
-      <MapContainer />
-    </div>
-  )
-};
+class App extends React.Component {
+  state = {
+    show: false,
+    locations: [],
+  };
+
+  componentDidMount() {
+    this.getResultsApi();
+  }
+
+  // No tiene los presets para "presets": [ "es2015", "stage-0" ] para usar async-await o no me funciono a mi
+  // Se que no debe llevar comentarios, pero personalmente prefiero algo como create react app
+  // Porque no me centro en configuracion si no en desarrollo.
+  getResultsApi = () => {
+    const url = `http://localhost:3000/locations`;
+    const ResultLocations = axios.get(url);
+    ResultLocations.then(res => {
+      this.setState({
+        locations: res.data,
+      });
+    });
+  };
+
+  handleToogleMap = e => {
+    const estado = this.state;
+
+    e.preventDefault();
+    if (estado.show === false) {
+      this.setState({
+        show: true,
+      });
+    } else {
+      this.setState({
+        show: false,
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div className="Header__header--main">
+          <div className="Header__logo">
+            <h1 className="Header__logo--h1">Logo</h1>
+          </div>
+          <div className="Header__menu">
+            <ul>
+              <li>Inicio</li>
+              <li>Acerca de</li>
+              <li>
+                <a href="!#" onClick={this.handleToogleMap}>
+                  Contacto
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <MapContainer estado={this.state} />
+      </div>
+    );
+  }
+}
 
 export default App;
