268dbd1e622dabea774445b20d08c520ffda38dd
Manuel Melgarejo Saenz <manuelmelgarejo@gmail.com>
2019-10-07 17:19:16 -0500

    Solución problema 3

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,27 +1,30 @@
 import React from 'react';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
-const MapContainer = ({ google }) => {
+const MapContainer = ( {google, locations} ) => {
+  
   return (
     <Map
       google={google}
       zoom={4}
       initialCenter={{lat: 19.4267261, lng: -99.1718706 }}
     >
-      <Marker
-        name='Platzi HQ CDMX'
-        position={{ lat: 19.4267261, lng: -99.1718706 }}
-      />
-      <Marker
-        name='Platzi HQ Bogota'
-        position={{lat: 4.6560716, lng: -74.0595918}} 
-      />
-      <Marker />
+      { 
+        locations.map((sitio, index) =>{
+          return (
+            <Marker 
+              key={index}
+              name={sitio.venueName}
+              position={{lat: sitio.venueLat, lng: sitio.venueLon }}
+            />
+          )
+        })
+      }
     </Map>
   );
 }
 
 export default GoogleApiWrapper({
   apiKey: 'AIzaSyCmjvkXB_DMnBUNwxQztLMStyQmA_szbNw',
-  language: 'spanish'
+  language: 'spanish',
 })(MapContainer);
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from "../components/Button";
 import '../styles/containers/Button.styl';
 import MapContainer from "../components/MapContainer";
 import '../styles/containers/App.styl';
 
 const App = () => {
+  // sitios guarda el estado,  setSitios permite actualizar el estado (sitios)
+  const [ sitios, setSitios ] = useState([]);
+  useEffect(()=>{
+    fetch('http://localhost:3000/locations')
+      .then(response => response.json())
+      .then(data => setSitios(data));
+  },[]);
   return (
     <div className="App">
       <Button />
       <section>
-        <MapContainer />
+        <MapContainer locations={sitios} />
       </section>
     </div>
   )
