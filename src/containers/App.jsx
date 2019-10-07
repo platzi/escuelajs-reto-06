40a223b0f58eea5efb0a2b2d6a2901cd2cb14d44
Mauricio Garcia <mau200729@gmail.com>
2019-10-07 15:29:02 -0500

    Se agregan los retos 01-04

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
-import MapContainer from "../components/MapContainer";
+import React, { useState, useEffect } from 'react';
+import MapContainer from '../components/MapContainer';
 import '../styles/containers/App.styl';
 
 const App = () => {
+  const [markers, setMarkers] = useState([]);
+
+  useEffect(() => {
+    const urlMarkers = 'http://localhost:3000/locations';
+    fetch(urlMarkers)
+      .then(response => response.json())
+      .then(data => {
+        setMarkers(data);
+      });
+  }, []);
+
+  // console.table(markers);
+
   return (
     <div className="App">
-      <MapContainer />
+      <MapContainer markers={markers} />
     </div>
-  )
+  );
 };
 
 export default App;
