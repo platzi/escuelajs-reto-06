6090afbb64170629fe9614a7857b225990e9e0d4
cesar galindo <csgalindos@hotmail.com>
2019-10-06 19:48:38 -0500

    retos

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,11 +1,21 @@
 /* eslint-disable react/button-has-type */
-import React, {useState} from 'react';
+import React, {useState,useEffect} from 'react';
 import MapContainer from "../components/MapContainer";
 import '../styles/containers/App.styl';
+
+const API = 'http://localhost:3000/locations';
 
 const App = () => {
   const [show,setShow] =  useState(true)
   const handleClick = () => {setShow(!show)}
+
+  useEffect(() => {
+    // eslint-disable-next-line no-undef
+    fetch(API)
+      .then(response => response.json())
+      // eslint-disable-next-line no-undef
+      .then(data => setLocations(data));
+  },[]);
 
   return (
     <div className='App'>
