6d8b8bdb9b3d3eddfcec6c0a8067eb16f95acfdf
William Velazquez <34907456+WilliamVelazquez@users.noreply.github.com>
2019-10-07 02:26:54 -0500

    Solving 1st Problem
    Switch MapContainer from functional to class component.
    Handling Show/Hide Button with Component State.

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
-const MapContainer = ({ google }) => {
-  return (
-    <Map
-      google={google}
-      zoom={5}
-      initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
-    >
-      <Marker
-        position={{ lat: 19.4267261, lng: -99.1718706 }}
-      />
-    </Map>
-  );
+class MapContainer extends Component {
+  state = {
+    show: false,
+  }
+  toggleMapVisibility = () => {
+    this.setState({show:!this.state.show})
+  }
+  render(){
+    const { google } = this.props;
+    const { show } = this.state;
+    return (
+      <>
+        <button onClick={this.toggleMapVisibility}>{show?'Hide':'Show map'}</button>
+        {
+          show &&
+          <Map
+            google={google}
+            zoom={5}
+            initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
+          >
+            <Marker
+              position={{ lat: 19.4267261, lng: -99.1718706 }}
+            />
+          </Map>
+        }
+      </>
+    );
+  }
 }
 
 export default GoogleApiWrapper({
