3cc8483bddb80644afd316bdf41e39ab71e62783
cristubuntu <cristhiam.tovar@mail.escuelaing.edu.co>
2019-10-08 16:43:23 -0500

    it's groosly working

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -33,15 +33,21 @@ class MapContainer extends Component {
   }
   
   hidder(){
-    this.setState({
-      show: false
-    })
+    if(this.state.show===false){
+      this.setState({
+          show: true
+      });
+    }else{
+      this.setState({
+        show: false
+    });
+    }
   }
 
   render() {
     return (
       <div>
-        <button onClick={()=>this.hidder()}>Hide Map</button>
+        <button onClick={()=>this.hidder()}>show Map</button>
         {
           this.state.show?
             <Map
