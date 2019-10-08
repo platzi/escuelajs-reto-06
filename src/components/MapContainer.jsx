a5cce8daed13c9007a42d814e48388ca5f547742
Edwin García <spark.com.co@gmail.com>
2019-10-08 14:47:35 -0500

    Primer problema

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
-const MapContainer = ({ google }) => {
-  return (
-    <Map
-      google={google}
-      zoom={5}
-      initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
-    >
-      <Marker
-        position={{ lat: 19.4267261, lng: -99.1718706 }}
-      />
-    </Map>
-  );
+class MapContainer extends Component {
+  constructor(){
+    super();
+    this.state = {
+      show: true
+    }
+  }
+
+  render(){
+    const handleButton = () => {
+      this.setState((state) => {
+        return {show: !state.show}
+      })
+    }
+
+    return (
+      <>
+        <button onClick={handleButton}>{this.state.show ? 'ocultar mapa' : 'mostrar mapa'}</button>
+        
+        {this.state.show && (
+        <Map
+          google={google}
+          zoom={5}
+          initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
+        >
+          <Marker
+            position={{ lat: 19.4267261, lng: -99.1718706 }}
+          />
+        </Map>
+)}
+      </>
+    );
+  }
 }
 
 export default GoogleApiWrapper({
