516cb4c085077215be937c50f7bbc56da383f9e1
Jorge Humberto Nemoga Pinzon <jhnemogap@gmail.com>
2019-10-08 10:22:00 -0500

    Second problem solved. Direct change - simple form

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -11,6 +11,9 @@ const MapContainer = ({ google }) => {
       <Marker
         position={{ lat: 19.4267261, lng: -99.1718706 }}
       />
+      <Marker
+        position={{ lat: 4.6560716, lng: -74.0595918 }}
+      />
     </Map>
   );
 }
