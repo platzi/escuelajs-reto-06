9944dd7f06ebb53c74517030ef631084d89ccebb
Alejandro Cortez <ziker@live.com.mx>
2019-10-07 21:34:27 -0500

    6th challenge finished

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,13 +1,43 @@
-import React from 'react';
-import MapContainer from "../components/MapContainer";
+import React, { Component } from 'react';
+import MapContainer from '../components/MapContainer';
 import '../styles/containers/App.styl';
 
-const App = () => {
-  return (
-    <div className="App">
-      <MapContainer />
-    </div>
-  )
-};
+const API = 'http://localhost:3000/locations';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showMap: false,
+      label: 'Mostrar Mapa',
+      data: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch(API)
+      .then((response) => response.json())
+      .then((data) => this.setState({ data }));
+  }
+
+  handleClick = () => {
+    const {showMap} = this.state;
+    const _label = !showMap ? 'Ocultar Mapa' : 'Mostrar Mapa';
+    this.setState(state => ({
+      showMap: !state.showMap,
+      label: _label,
+    }));
+  };
+
+  render() {
+    const {label, showMap, data} = this.state;
+    return (
+      <div className="App">
+        <button type="button" onClick={this.handleClick}>{label}</button>
+        <MapContainer showMap={showMap} locations={data} />
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/hooks/useInitialState.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'react';
+
+const useInitialState = (API) => {
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    fetch(API)
+      .then((response) => response.json())
+      .then((data) => setVideos(data));
+  }, []);
+
+  return videos;
+};
+
+export default useInitialState;
