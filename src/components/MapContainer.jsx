89afaf2d3f6ebaf0b52c2b1e9b15616edaf6fe8b
Erik <erik@forwardcompany.com>
2019-10-08 14:18:30 -0500

    update reto 3

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -8,14 +8,7 @@ class MapContainer extends React.Component {
       showLocation: false,
       marker: {},
       place: {},
-      locations: [],
     }
-  }
-
-  componentDidMount() {
-    fetch('http://localhost:3000/locations')
-      .then(response => response.json())
-      .then(json => this.setState({locations: json}));
   }
   
   onMarkerClick(props, marker, e) {
@@ -36,14 +29,14 @@ class MapContainer extends React.Component {
   }
 
   render() {
-    const { google } = this.props;
+    const { google, locations } = this.props;
     return (
       <Map google={google}
         onClick={this.onMapClicked}
         zoom={5}
         initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
       >
-        {this.state.locations.map((location, key) => (
+        {locations.map((location, key) => (
           <Marker key={key} onClick={(props, marker, e) => this.onMarkerClick(props, marker, e)}
             position={{ lat: location.venueLat, lng: location.venueLon }}
             name={location.venueName}
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,15 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import MapContainer from "../components/MapContainer";
 import '../styles/containers/App.styl';
 
 const App = () => {
   const [show, changeShow] = useState(false);
+  const [locations, addLocations] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/locations')
+      .then(response => response.json())
+      .then(json => addLocations(json));
+  },[])
+
   return (
     <div className="App">
       <button onClick={() => changeShow(!show)}>
         {show ? 'Ocultar' : 'Mostrar'} Mapa
       </button>
-      {show && <MapContainer /> }
+      {show && <MapContainer locations={locations} /> }
     </div>
   )
 };
