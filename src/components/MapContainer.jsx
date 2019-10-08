d3d5cd5233de176c14ef98e6e3e803c872b61969
Edwin García <spark.com.co@gmail.com>
2019-10-08 17:16:20 -0500

    Segundo problema

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
 class MapContainer extends Component {
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
     this.state = {
       show: true
     }
@@ -22,12 +22,19 @@ class MapContainer extends Component {
         
         {this.state.show && (
         <Map
-          google={google}
+          google={this.props.google}
           zoom={5}
           initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
         >
           <Marker
+            title="Platzi HQ México"
+            name="Platzi HQ México"
             position={{ lat: 19.4267261, lng: -99.1718706 }}
+          />
+          <Marker
+            title="Platzi HQ Bogotá"
+            name="Platzi HQ Bogotá"
+            position={{ lat: 4.6560716, lng: -74.0595918 }}
           />
         </Map>
 )}
