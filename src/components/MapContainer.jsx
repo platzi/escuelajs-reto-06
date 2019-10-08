471074c6f1ad69b227dbe57777a869439f276a18
Francisco Angel <paconity@gmail.com>
2019-10-08 18:04:03 -0500

    Tercer Problema Resuelto

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,23 +1,15 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
-/* 
-const MapContainer = ({ google }) => {
-  return (
-    <Map
-      google={google}
-      zoom={5}
-      initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
-    >
-      <Marker
-        position={{ lat: 19.4267261, lng: -99.1718706 }}
-      />
-    </Map>
-  );
-} */
 
 const MapContainer = ({ google }) => {
-
   const [showMap, setShowMap] = useState(false);
+  const [location, setLocation] = useState([]);
+
+  useEffect (() => {
+    fetch('http://localhost:3000/locations')
+      .then(response => response.json())
+      .then(data => setLocation(data));
+  }, []);
 
   if (showMap === true){
     return (
@@ -26,12 +18,12 @@ const MapContainer = ({ google }) => {
         zoom={5}
         initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
       >
-        <Marker
-          position={{ lat: 19.4267261, lng: -99.1718706 }}
-        />
-        <Marker
-          position={{ lat: 4.6560716, lng: -74.0595918 }}
-        />
+        {location.map((item) => (
+          <Marker
+            key={item.id}
+            position={{ lat: item.venueLat, lng: item.venueLon }}
+          />
+        ))}
       </Map>
     )
   }
