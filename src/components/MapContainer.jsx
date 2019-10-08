5154100a6fb9a60376519508041c042f978ef840
proartmateur <ennimandroid@gmail.com>
2019-10-08 17:03:50 -0500

    Usando InfoWindow

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,77 +1,84 @@
-import React, {Component} from 'react';
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
 
 class MapContainer extends Component {
+
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       google: this.props.google,
       visible: true,
-      text: "Ocultar",
-      points: this.props.points
+      text: 'Ocultar',
+      points: this.props.points,
+      selectedPlace: {},
+      activeMarker: {},
+      showingInfoWindow: true,
     }
-    console.log("-- Maping points")
-    console.log(this.state.points);
-    
-    this.handleClick = this.handleClick.bind(this);
+
+    this.handleClick = this.handleClick.bind(this)
   }
 
-  handleClick () {
-    let caption = "Mostrar";
-    if(!this.state.visible) {
-      caption = "Ocultar";
+  onMarkerClick = (props, marker, e) => {
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true,
+    })
+  }
+
+  handleClick() {
+    let caption = 'Mostrar'
+    if (!this.state.visible) {
+      caption = 'Ocultar'
     } else {
-      caption = "Mostrar";
+      caption = 'Mostrar'
     }
     this.setState(state => ({
       visible: !state.visible,
-      text: caption
+      text: caption,
     }));
   }
 
-  render () {
-    /* var bounds = new this.props.google.maps.LatLngBounds();
-    for (var i = 0; i < points.length; i++) {
-      bounds.extend(points[i]);
-    } */
-
+  render() {
     return (
       <React.Fragment>
+        <button className="show" type="button" onClick={this.handleClick}>
+          {this.state.text}
+        </button>
 
-        <button className="show" type="button" onClick={this.handleClick}>{this.state.text}</button>
-        
-        {
-          this.state.visible && (
-            <Map
-              google={this.state.google}
-              zoom={5}
-              initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
+        {this.state.visible && (
+          <Map
+            google={this.state.google}
+            zoom={5}
+            initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
+          >
+            {this.state.points.map((point, i) => {
+              return (
+                <Marker
+                  onClick={this.onMarkerClick}
+                  key={`mark_${  i.toString() }`}
+                  name={`mark_${  i.toString() }`}
+                  title={point.venueName}
+                  position={{ lat: point.venueLat, lng: point.venueLon }}
+                />
+              );
+            })}
+
+            <InfoWindow
+              marker={this.state.activeMarker}
+              visible={this.state.showingInfoWindow}
             >
-              {
-                this.state.points.map( (point, i) => {
-                  return(
-                    <Marker
-                      key={"mark_" + i.toString()}
-                      name={"mark_" + i.toString()}
-                      title={point.venueName}
-                      position={{ lat: point.venueLat, lng: point.venueLon }}
-                    />
-                  )
-                })
-              }
-            </Map>
-          )
-          
-        }
-  
-        
+              <div>
+                <h2>{this.state.selectedPlace.title}</h2>
+              </div>
+            </InfoWindow>
+          </Map>
+        )}
       </React.Fragment>
     );
   }
-  
-};
-
+}
 
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyCmjvkXB_DMnBUNwxQztLMStyQmA_szbNw'
+  apiKey: 'AIzaSyCmjvkXB_DMnBUNwxQztLMStyQmA_szbNw',
 })(MapContainer);
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,44 +1,39 @@
-import React, {Component} from 'react';
-import MapContainer from "../components/MapContainer";
+import React, { Component } from 'react';
+import MapContainer from '../components/MapContainer';
 import '../styles/containers/App.styl';
 
-
-
 class App extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       locations: [],
-      api_url: "http://localhost:3000/locations"
-    }
-
-
+      api_url: 'http://localhost:3000/locations',
+    };
   }
 
   componentDidMount() {
     fetch(this.state.api_url)
-    .then( (response) => {
-      return response.json()
-    })
-    .then( (data) => {
-      this.setLocations(data)
-    })
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        this.setLocations(data);
+      });
   }
 
   setLocations(locations_array) {
     this.setState({
-      locations: locations_array
-    })
+      locations: locations_array,
+    });
   }
 
-  render(){
+  render() {
     return (
       <div className="App">
         <MapContainer points={this.state.locations} />
       </div>
-    )
+    );
   }
 }
-
 
 export default App;
