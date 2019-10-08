3ae973282402ea197937ab7b90c4f373bee31440
Alejandro Lopez <alejozepol@gmail.com>
2019-10-08 17:34:00 -0500

    Creacion de infowindws para el map

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import CardMap from './CardMap';
 import FormContact from './FormContact';
 import MapContainer from "./MapContainer";
 import '../styles/containers/Contact.styl';
 
 const Contact = () => {
+  const [show, setShow] = useState(true)
   return (
     <section className='Contact'>
       <div className='Contact__info'>
+        <button className='Contact__info__boton' onClick={() => {setShow(!show)}} > {show ? 'Ocultar' : 'Mostrar'}</button>
         <FormContact />
         <h3 className='Contact__title'>Oficinas</h3>
         <CardMap name='Platzi Mexico' />
         <CardMap name='Platzi Bogotá' />
       </div>
-      <MapContainer />
+      {show && <MapContainer />} 
     </section>
   )
 };
--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,19 +1,60 @@
-import React from 'react';
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
 
-const MapContainer = ({ google }) => {
+export class MapContainer extends Component{
+  
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {},
+  };
+  
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
 
-  return (
-    <Map
-      google={google}
-      zoom={5}
-      initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
-    >
-      <Marker
-        position={{ lat: 19.4267261, lng: -99.1718706 }}
-      />
-    </Map>
-  );
+    onMapClicked = (props) => {
+      if (this.state.showingInfoWindow) {
+        this.setState({
+          showingInfoWindow: false,
+          activeMarker: null
+        })
+      }
+    };
+
+    render() {
+      return (
+        <Map
+          google={google}
+          zoom={3}
+          initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
+          onClick={this.onMapClicked}
+        >
+
+          <Marker 
+            onClick={this.onMarkerClick}
+            name={ 'Platzi HQ Mexico' }
+            position={{ lat: 19.5943885, lng: -97.9526044 }}
+          />
+          <Marker 
+            onClick={this.onMarkerClick}
+            name={ 'Platzi HQ Bogota' }
+            position={{ lat: 4.6560716, lng: -74.0595918 }}
+          />
+
+          <InfoWindow
+            marker={ this.state.activeMarker }
+            visible={ this.state.showingInfoWindow }>
+            <div>
+              <h1>{ this.state.selectedPlace.name}</h1>
+            </div>
+          </InfoWindow>
+        </Map>
+      );
+    }
 }
 
 export default GoogleApiWrapper({
