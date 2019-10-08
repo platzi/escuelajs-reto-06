fea7b5c65f12aeba3d946c6b1cc91a605cf11a43
Francisco Angel <paconity@gmail.com>
2019-10-08 12:35:49 -0500

    Primer Problema Resuelto

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
-
+/* 
 const MapContainer = ({ google }) => {
   return (
     <Map
@@ -12,6 +12,28 @@ const MapContainer = ({ google }) => {
         position={{ lat: 19.4267261, lng: -99.1718706 }}
       />
     </Map>
+  );
+} */
+
+const MapContainer = ({ google }) => {
+
+  const [showMap, setShowMap] = useState(false);
+
+  if (showMap === true){
+    return (
+      <Map
+        google={google}
+        zoom={5}
+        initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
+      >
+        <Marker
+          position={{ lat: 19.4267261, lng: -99.1718706 }}
+        />
+      </Map>
+    )
+  }
+  return (
+    <div><button type='button' onClick={() => setShowMap(true)}>Mostrar Mapa</button></div>
   );
 }
 
