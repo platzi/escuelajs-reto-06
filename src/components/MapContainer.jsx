fe20153285f65d1e56f37d6d2ff983df7ab2bfa4
Inti <yeseniase@outlook.it>
2019-10-08 14:46:05 -0500

    ADD: solución 1er reto

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,20 +1,43 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
-const MapContainer = ({ google }) => {
-  return (
-    <Map
-      google={google}
-      zoom={5}
-      initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
-    >
-      <Marker
-        position={{ lat: 19.4267261, lng: -99.1718706 }}
-      />
-    </Map>
-  );
-}
+class MapContainer extends Component {
+  state = {
+    show: false,
+  }
+
+  handleClick = () => {
+    this.setState({
+      show: !this.state.show
+    });
+  }
+
+  render() {
+    const { show } = this.state;
+    return(
+      <Fragment>
+        <button type='button' onClick={this.handleClick}>
+          {show ? 'Ocultar mapa' : 'Mostrar mapa'}
+        </button>
+        {
+          show &&
+          <Map
+            google={google}
+            zoom={5}
+            initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
+            className='map'
+          >
+            <Marker
+              position={{ lat: 19.4267261, lng: -99.1718706 }}
+            />
+          </Map>
+        }
+      </Fragment>
+    );
+  }
+};
 
 export default GoogleApiWrapper({
   apiKey: 'AIzaSyCmjvkXB_DMnBUNwxQztLMStyQmA_szbNw'
 })(MapContainer);
+
