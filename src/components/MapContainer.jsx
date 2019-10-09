f7e8896d43f289ac0c541f00e9644a677ac179d7
Fabian Toro <helber.toro@gmail.com>
2019-10-08 21:19:05 -0500

    Challenge 1

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
-const MapContainer = ({ google }) => {
-  return (
-    <Map
-      google={google}
-      zoom={5}
-      initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
-    >
-      <Marker
-        position={{ lat: 19.4267261, lng: -99.1718706 }}
-      />
-    </Map>
-  );
+class MapContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false,
+    }
+  }
+
+  toogleButton = () => {
+    this.setState({
+      show: !this.state.show
+    })
+  }
+
+  render() {
+    const {show} = this.state;
+    return(
+      <div>
+        <button type="button" onClick={this.toogleButton}>{show ? 'Ocultar mapa' : 'Ver mapa' }</button>
+        {show &&
+          <Map
+          google={google}
+          zoom={5}
+          initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
+          >
+            <Marker
+            position={{ lat: 19.4267261, lng: -99.1718706 }}
+            />
+          </Map>
+        }
+      </div>
+    )
+  }
 }
 
 export default GoogleApiWrapper({
