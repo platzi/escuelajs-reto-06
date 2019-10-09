fe587221b755f8dbc9b0e92371213c707c60aec1
Inti <yeseniase@outlook.it>
2019-10-09 13:35:17 -0500

    ADD: solución 4to problema

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,9 +1,12 @@
 import React, { Component, Fragment } from 'react';
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
 
 class MapContainer extends Component {
   state = {
     show: false,
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {},
   }
 
   handleClick = () => {
@@ -12,30 +15,50 @@ class MapContainer extends Component {
     });
   }
 
+  onMarkerClick = (props, marker, e) => {
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+  }
+
   render() {
     const { google, locations } = this.props;
-    const { show } = this.state;
+    const { show, activeMarker, showingInfoWindow, selectedPlace } = this.state;
     return(
       <Fragment>
         <button type='button' className='button' onClick={this.handleClick}>
           {show ? 'Ocultar mapa' : 'Mostrar mapa'}
         </button>
-        {
-          show &&
+        { show &&
           <Map
             google={google}
             zoom={4}
+            initialCenter={{ lat: 19.42672619, lng: -99.1718706 }}
             className='map'
           >
             {
-              locations.map( location =>
+              locations.map( location => (
+              //  return <>
                 <Marker
-                  key= {location.venuName}
-                  name={location.venueName}
-                  position={{ lat: location.venueLat, lng: location.venueLon }}
+                key= {location.venueName}
+                name={location.venueName}
+                position={{ lat: location.venueLat, lng: location.venueLon }}
+                onClick={this.onMarkerClick}
                 />
-              )
+              ))
             }
+           {
+              <InfoWindow
+              marker={activeMarker}
+              visible={showingInfoWindow}
+              >
+                <div>
+                  <p><strong>{selectedPlace.name}</strong></p>
+                </div>
+              </InfoWindow>
+          }
           </Map>
         }
       </Fragment>
