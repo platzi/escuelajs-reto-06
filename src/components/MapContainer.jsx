4663542219e276c5b9eb9eda91c7d70f74f625b9
Zahira <zahira.rosales@gmail.com>
2019-10-09 12:57:54 -0500

    Tercer Reto

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -3,18 +3,16 @@ import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
 class MapContainer extends React.Component{
   render(){
+    const {locations} = this.props;
     return (
       <Map
         google={google}
         zoom={5}
         initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
       >
-        <Marker
-          position={{ lat: 19.4267261, lng: -99.1718706 }}
-        />
-        <Marker
-        position={{ lat: 4.6560716, lng: -74.0595918 }}
-        />
+        { locations.map((location, item) => 
+          <Marker key={item} position={{ lat: location.venueLat, lng: location.venueLon }} /> 
+        )}
       </Map>
     );
   }
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,10 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect } from 'react';
 import MapContainer from "../components/MapContainer";
 import '../styles/containers/App.styl';
 
-const App = () => {
-  const [show, setShow] = useState(false);
+const API = 'http://localhost:3000/locations'
 
+const App = () => {
+  
+  const [locations, setMarker] = useState([]);
+    useEffect(() => {
+      fetch(API)
+      .then(response => response.json())
+      .then(dataLocations => setMarker(dataLocations))
+    }, [])
+
+  const [show, setShow] = useState(false);
   const handleClick = () => {
     setShow(!show);
   };
@@ -12,7 +21,7 @@ const App = () => {
   return (
     <div className="App">
       <button onClick={handleClick}>{show ? 'Hide' : 'Show'}</button>
-      {show && <MapContainer />}
+      {show && <MapContainer locations={locations} />}
     </div>
   )
 };
