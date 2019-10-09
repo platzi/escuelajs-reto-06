a078c938d8d9d4129f75a45dc9b2ed946d86e909
Fabian Toro <helber.toro@gmail.com>
2019-10-08 22:47:55 -0500

    Challenge 4

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
 
 class MapContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
       show: false,
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {},
     }
   }
 
@@ -14,6 +17,22 @@ class MapContainer extends Component {
       show: !this.state.show
     })
   }
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+ 
+  onMapClicked = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      })
+    }
+  };
 
   render() {
     const {locations} = this.props;
@@ -26,13 +45,22 @@ class MapContainer extends Component {
           google={this.props.google}
           zoom={5}
           initialCenter={{ lat: 11.8901835, lng: -84.0547899 }}
+          onClick={this.onMapClicked}
           >
             {locations.map(item => 
               <Marker
+              onClick={this.onMarkerClick}
               name={item.venueName}
               position={{ lat: item.venueLat, lng: item.venueLon }}
               />
             )}
+            <InfoWindow
+              marker={this.state.activeMarker}
+              visible={this.state.showingInfoWindow}>
+              <div>
+                <h1>{this.state.selectedPlace.name}</h1>
+              </div>
+            </InfoWindow>
           </Map>
         }
       </div>
