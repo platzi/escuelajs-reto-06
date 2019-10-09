0ea39be574cccc528448442801690ae7260bf120
wilantury <ventas@antury.net>
2019-10-09 07:05:50 -0500

    Solución problema 1

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,18 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
-const MapContainer = ({ google }) => {
-  return (
-    <Map
-      google={google}
-      zoom={5}
-      initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
-    >
-      <Marker
-        position={{ lat: 19.4267261, lng: -99.1718706 }}
-      />
-    </Map>
-  );
+class MapContainer extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      show: true,
+    };
+  } 
+
+  handleShowMap = () => {
+    const { show } = this.state;
+    if(show){
+      this.setState({
+        show: false,
+      })
+    }else{
+      this.setState({
+        show: true,
+      })
+    }     
+  }
+
+  render(){
+    const {google} = this.props;
+    const {show} = this.state;
+    return (
+      <>
+        <button type="button" onClick={this.handleShowMap}>
+          mostrar/ocultar mapa
+        </button>
+        <Map
+          visible={show} 
+          google={google}
+          zoom={5}
+          initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
+        >
+          <Marker
+            position={{ lat: 19.4267261, lng: -99.1718706 }}
+          />
+        </Map>
+      </>
+    );
+  }
+  
 }
 
 export default GoogleApiWrapper({
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import MapContainer from "../components/MapContainer";
+import MapContainer from '../components/MapContainer';
 import '../styles/containers/App.styl';
 
 const App = () => {
