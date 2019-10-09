b0e1e96f77206fc1b5685340f896460bfd2d807c
FrcGustavo <hidalgo_fco520@outlook.com>
2019-10-09 17:20:27 -0500

    Fourth challenge completed

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,28 +1,60 @@
-import React from 'react';
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import React, { useState } from 'react';
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
 
 const MyMap = ({ google, markers }) => {
-  console.log(markers);
-    return (
-      <Map
-        google={google}
-        zoom={4}
-        initialCenter={{ lat: 19.5943885, lng: -84.9526044 }}
+  const [ place, setPlace ] = useState({
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {},
+  });
+
+  const onMarkerClick = (props, marker) => {
+    setPlace({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true,
+    });
+  }
+
+  const onMapClicked = () => {
+    if (place.showingInfoWindow) {
+      setPlace({
+        showingInfoWindow: false,
+        activeMarker: null,
+        selectedPlace: {}
+      });
+    }
+  };
+  return (
+    <Map
+      google={google}
+      zoom={4}
+      initialCenter={{ lat: 19.5943885, lng: -84.9526044 }}
+      onClick={onMapClicked}
+    >
+      {
+        markers.map((marker, idx) => {
+          const { venueLat, venueLon, venueName } = marker;
+          return (
+            <Marker
+              key={idx}
+              name={venueName} 
+              position={{ lat: venueLat, lng: venueLon }}
+              onClick={onMarkerClick}
+            />
+          )
+        })
+      }
+      <InfoWindow
+        marker={place.activeMarker}
+        visible={place.showingInfoWindow}
       >
-        {
-          markers.map((marker, idx) => {
-            const { venueLat, venueLon, venueName } = marker;
-            return (
-              <Marker
-                key={idx}
-                name={venueName} 
-                position={{ lat: venueLat, lng: venueLon }}
-              />
-            )
-          })
-        }
-      </Map>
-    )
+        <div>
+          <h1>{place.selectedPlace.name}</h1>
+        </div>
+      </InfoWindow>
+    </Map>
+  )
 }
 
 export default GoogleApiWrapper({
