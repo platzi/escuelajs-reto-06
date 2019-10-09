9fad1d645c45908d9e2fa1ffe80d48d1e3850817
Alexis Araujo <ala1289@gmail.com>
2019-10-09 17:07:46 -0500

    Tercer problema resuelto con hooks

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,33 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
-class MapContainer extends Component {
-  state = { show: true };
-
-  handleClick = () => {
-    this.setState({ show: !this.state.show });
-  };
-
-  render() {
-    return (
-      <>
-        <button onClick={this.handleClick}>Show!!!!</button>
-        {this.state.show ? (
-          <Map
-            google={this.props.google}
-            zoom={5}
-            initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
-          >
-            <Marker position={{ lat: 19.4267261, lng: -99.1718706 }} />
-            <Marker position={{ lat: 4.6560716, lng: -74.0595918 }} />
-          </Map>
-        ) : (
-          <h1>Para mostrar el mapa necesitas presionar el boton ☝🏼</h1>
-        )}
-      </>
-    );
-  }
-}
+const MapContainer = ({ google, locations }) => {
+  return (
+    <Map
+      google={google}
+      zoom={5}
+      initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
+    >
+      {locations.map(item => {
+        const { id, venueName, venueLat, venueLon } = item;
+        return (
+          <Marker
+            key={id}
+            title={venueName}
+            position={{ lat: venueLat, lng: venueLon }}
+          />
+        );
+      })}
+    </Map>
+  );
+};
 
 export default GoogleApiWrapper({
   apiKey: 'AIzaSyCmjvkXB_DMnBUNwxQztLMStyQmA_szbNw',
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import MapContainer from '../components/MapContainer';
 import '../styles/containers/App.styl';
 
+const API = 'http://localhost:3000/locations';
+
 const App = () => {
+  const [locations, setLocations] = useState([]);
+  const [btnTxt, setBtnTxt] = useState('Show Map!');
+  const [visibility, setVisibility] = useState(false);
+
+  const handleClick = () => {
+    if (visibility === false) {
+      setBtnTxt('Hide Map!');
+      setVisibility(true);
+    } else {
+      setBtnTxt('Show Map!');
+      setVisibility(false);
+    }
+  };
+
+  useEffect(() => {
+    window
+      .fetch(API)
+      .then(response => response.json())
+      .then(data => setLocations(data));
+  }, []);
+
   return (
     <div className="App">
-      <MapContainer />
+      <button type="button" onClick={handleClick}>
+        {btnTxt}
+      </button>
+      {visibility ? (
+        <MapContainer locations={locations} />
+      ) : (
+        <h1>Para mostrar el mapa necesitas presionar el boton ☝🏼</h1>
+      )}
     </div>
   );
 };
