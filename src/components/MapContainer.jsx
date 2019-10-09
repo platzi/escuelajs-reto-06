934358b3faaea8d3a913123c9b3e09d74059c40d
Francisco Angel <paconity@gmail.com>
2019-10-09 17:40:14 -0500

    Primer problema Resuelto Correctamente

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
 const MapContainer = ({ google }) => {
-  const [showMap, setShowMap] = useState(false);
+  
   const [location, setLocation] = useState([]);
 
   useEffect (() => {
@@ -11,25 +11,21 @@ const MapContainer = ({ google }) => {
       .then(data => setLocation(data));
   }, []);
 
-  if (showMap === true){
-    return (
-      <Map
-        google={google}
-        zoom={5}
-        initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
-      >
-        {location.map((item) => (
-          <Marker
-            key={item.id}
-            position={{ lat: item.venueLat, lng: item.venueLon }}
-          />
-        ))}
-      </Map>
-    )
-  }
   return (
-    <div><button type='button' onClick={() => setShowMap(true)}>Mostrar Mapa</button></div>
-  );
+    <Map
+      google={google}
+      zoom={5}
+      initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
+    >
+      {location.map((item) => (
+        <Marker
+          key={item.id}
+          position={{ lat: item.venueLat, lng: item.venueLon }}
+        />
+      ))}
+    </Map>
+  )
+  
 }
 
 export default GoogleApiWrapper({
--- src/index.js
+++ src/index.js
@@ -3,4 +3,3 @@ import ReactDOM from 'react-dom';
 import App from './containers/App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
-ReactDOM.render(<App />, document.getElementById('btnMap'));
