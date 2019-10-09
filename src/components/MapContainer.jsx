b6bdef6ce1995dc67e11058db80506d9191da957
wilantury <ventas@antury.net>
2019-10-09 07:22:43 -0500

    Solución problema 2

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -25,6 +25,16 @@ class MapContainer extends Component {
   render(){
     const {google} = this.props;
     const {show} = this.state;
+
+    const points = [
+      { lat: 4.6560716, lng: -74.0595918 },
+    ]
+
+    const bounds = new this.props.google.maps.LatLngBounds();
+    for (let i = 0; i < points.length; i++) {
+      bounds.extend(points[i]);
+    }
+
     return (
       <>
         <button type="button" onClick={this.handleShowMap}>
@@ -35,9 +45,15 @@ class MapContainer extends Component {
           google={google}
           zoom={5}
           initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
+          bounds={bounds}
         >
           <Marker
             position={{ lat: 19.4267261, lng: -99.1718706 }}
+          />
+          <Marker
+            title={'Platzi HQ Bogotá'}
+            name={'Platzi Bogota'}
+            position={{lat: 4.6560716, lng: -74.0595918}} 
           />
         </Map>
       </>
