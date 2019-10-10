45841087b802cdcbb0334c46f26e1aa3094c789e
Oscar Lara <oscar.lara@2bcore.com>
2019-10-09 21:39:48 -0500

    Segundo problema resuelto.

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -29,7 +29,8 @@ export class MapContainer extends React.Component {
               zoom={5}
               initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
             >
-              <Marker position={{ lat: 19.4267261, lng: -99.1718706 }}/>
+              <Marker position={{ lat: 19.4267261, lng: -99.1718706 }} />
+              <Marker position={{ lat: 4.6560716, lng: -74.0595918 }} />
             </Map>
         ) : (
           <div>
