c2475001878513a2c3514a8939cdfaf404cca309
jeisson <jeisson.misnaza@gdx.travel>
2019-10-09 23:39:56 -0500

    solucion reto 5

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,18 +1,84 @@
-import React from 'react';
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
+import '../styles/containers/MapContainer.styl';
 
-const MapContainer = ({ google }) => {
-  return (
-    <Map
-      google={google}
-      zoom={5}
-      initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
-    >
-      <Marker
-        position={{ lat: 19.4267261, lng: -99.1718706 }}
-      />
-    </Map>
-  );
+class MapContainer extends Component {
+  constructor(props) {
+    super(props);
+    const { data } = props
+    this.state = {
+      show: false,
+      markers: data,
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {},
+    };
+  }
+
+  handleMapVisibility = () => {
+    const { show } = this.state;
+    if (show) {
+      this.setState({ show: false });
+    } else {
+      this.setState({ show: true });
+    }
+  };
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true,
+    });
+
+  onMapClick = props => {
+    const { showingInfoWindow } = this.state;
+    if (showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null,
+      });
+    }
+  };
+
+
+  render() {
+    const {
+      show,
+      markers,
+      activeMarker,
+      showingInfoWindow,
+      selectedPlace,
+    } = this.state;
+    const { google } = this.props;
+    return (
+      <div>
+        <button type="button" className="button" onClick={this.handleMapVisibility}>
+          {!show ? 'Mostrar mapa' : 'Ocultar Mapa'}
+        </button>
+        <Map          
+          google={google}
+          zoom={5}
+          initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
+          visible={show}
+          onClick={this.onMapClick}
+        >
+          {markers.map(item => (
+            <Marker
+              key={item.venueName}
+              name={item.venueName}
+              position={{ lat: item.venueLat, lng: item.venueLon }}
+              onClick={this.onMarkerClick}
+            />
+          ))}
+          <InfoWindow marker={activeMarker} visible={showingInfoWindow}>
+            <div>
+              <h1>{selectedPlace.name}</h1>
+            </div>
+          </InfoWindow>
+        </Map>
+      </div>
+    );
+  }
 }
 
 export default GoogleApiWrapper({
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import MapContainer from "../components/MapContainer";
+import MapContainer from '../components/MapContainer';
 import '../styles/containers/App.styl';
+import useInitialState from '../hooks/useInitialState';
+
+const API = 'http://localhost:3000/locations';
 
 const App = () => {
-  return (
+
+  const initialState = useInitialState(API);
+  return initialState.length === 0 ? (
+    <h1>Cargando...</h1>
+  ) : (
     <div className="App">
-      <MapContainer />
+      <MapContainer data={initialState} />
     </div>
-  )
+    );
 };
 
 export default App;
