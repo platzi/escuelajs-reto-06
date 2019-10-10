76a918b2428d5653936e24d254fae387ffa28c4a
BereSoto <beresoto.f@gmail.com>
2019-10-09 23:29:42 -0500

    mostar mapa

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,13 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import MapContainer from "../components/MapContainer";
 import '../styles/containers/App.styl';
+import '../styles/main.css';
 
-const App = () => {
-  return (
-    <div className="App">
-      <MapContainer />
-    </div>
-  )
-};
+
+class App extends Component {
+  state = {
+    show: false,
+  }
+
+
+  handleToggleShow = () => {
+    this.setState({
+      show: !this.state.show
+    });
+  }
+
+
+  render() {
+    const MAP = this.state.show ? <MapContainer /> : '';
+
+
+    return (
+      <div className="App">
+        { MAP }
+
+        <button
+          className={`btn  ${this.state.show ? 'btn--is-active-view': 'btn--is-desactive-view'}`}
+          onClick={this.handleToggleShow}
+        >
+          Mostrar mapa
+        </button>
+
+        <button
+          className={`btn  btn--hidden  ${!this.state.show ? 'btn--is-active-hidden': 'btn--is-desactive-hidden'}`}
+          onClick={this.handleToggleShow}
+        >
+          Ocultar mapa
+        </button>
+      </div>
+    )
+  }
+}
 
 export default App;
+
