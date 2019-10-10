46a7bfaade3750fb053e65d745af86c6f17731e8
Luis Caraballo <luisred61@gmail.com>
2019-10-09 21:11:46 -0500

    First Challenge Completed

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,18 +1,38 @@
 import React from 'react';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
-const MapContainer = ({ google }) => {
-  return (
-    <Map
-      google={google}
-      zoom={5}
-      initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
-    >
-      <Marker
-        position={{ lat: 19.4267261, lng: -99.1718706 }}
-      />
-    </Map>
-  );
+class MapContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { show: false };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(state => ({
+      show: !state.show
+    }));
+  }
+
+  render() {
+    return (
+      <>
+        <button className="button" onClick={this.handleClick}>
+          {this.state.show ? 'Ocultar' : 'Mostrar'}
+        </button>
+        <Map
+          google={google}
+          zoom={5}
+          initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
+          visible={this.state.show}
+        >
+          <Marker
+            position={{ lat: 19.4267261, lng: -99.1718706 }}
+          />
+        </Map>
+      </>
+    )
+  }
 }
 
 export default GoogleApiWrapper({
