38e749963c31e11ef768679aa982f22f50af5471
Diego Cruz <abeja.dact_@hotmail.com>
2019-10-10 11:55:15 -0500

    segundo reto terminado

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,19 +1,32 @@
 import React from 'react';
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
 
-const MapContainer = ({ google }) => {
-  return (
-    <Map
-      google={google}
-      zoom={5}
-      initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
-    >
-      <Marker
-        position={{ lat: 19.4267261, lng: -99.1718706 }}
-      />
-    </Map>
-  );
+ // eslint-disable-next-line react/prefer-stateless-function
+ class MapContainer extends React.Component{
+  
+  render(){
+    return (
+      <Map
+        // eslint-disable-next-line react/destructuring-assignment
+        google={this.props.google}
+        zoom={5}
+        initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
+      >
+        <Marker
+          name="Platzi Mexico"
+          position={{ lat: 19.4267261, lng: -99.1718706 }}
+        />
+        <Marker
+          name="Platzi Bogota"
+    // eslint-disable-next-line react/jsx-indent-props
+    position={{lat:  4.6560716, lng: -74.0595918}} />
+   
+ 
+      </Map>
+    );
+  }
 }
+
 
 export default GoogleApiWrapper({
   apiKey: 'AIzaSyCmjvkXB_DMnBUNwxQztLMStyQmA_szbNw'
