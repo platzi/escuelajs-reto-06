4f7f6cd027198c2c815bb787d265ed584c8afba5
Gerardo Nava Pereda <gerardo.nava.pereda@gmail.com>
2019-10-09 22:29:31 -0500

    Solved second challenge. Add Marker Platzi Bogota and México

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
+const markers = [
+  {
+    name: 'Platzi CDMX',
+    lat: 19.4267261,
+    lng: -99.1718706,
+  },
+  {
+    name: 'Platzi Bogotá',
+    lat: 4.6560716,
+    lng: -74.0595918,
+  },
+];
+
 class MapContainer extends Component {
   constructor(props) {
     super(props);
@@ -27,7 +40,9 @@ class MapContainer extends Component {
             zoom={5}
             initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
           >
-            <Marker position={{ lat: 19.4267261, lng: -99.1718706 }} />
+            {markers.map(marker => (
+              <Marker position={{ lat: marker.lat, lng: marker.lng }} />
+            ))}
           </Map>
         )}
       </>
