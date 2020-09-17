a77e2503be8bac45fac65d5469f1ac34b841f7c7
Daniel Santos <dsantos0495@gmail.com>
2020-09-16 22:48:08 -0500

    Retos 1 al 3 terminados, trabajdno en el 4to

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,19 +1,65 @@
+/* eslint-disable react/no-array-index-key */
 import React from 'react';
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
 
-const MapContainer = ({ google }) => {
-  return (
-    <Map
-      google={google}
-      zoom={5}
-      initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
-    >
-      <Marker
-        position={{ lat: 19.4267261, lng: -99.1718706 }}
-      />
-    </Map>
-  );
-}
+class MapContainer extends React.Component {
+
+  state = {
+    show: false,
+  }
+
+  handleBtnHide = () => {
+
+    const { show } = this.state;
+
+    if (show) {
+      this.setState({
+        show: false,
+      })
+    } else {
+      this.setState({
+        show: true,
+      });
+    }
+  }
+
+  render() {
+
+    const { google, coord } = this.props;
+    const { show } = this.state;
+    
+    return(
+      <>
+        <Map
+          google={google}
+          zoom={5}
+          initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
+          visible={show}
+        >
+          {coord.map((item, i) => (
+            <Marker
+              position={{ lat: item.venueLat, lng: item.venueLon }}
+              key={i}
+            >
+              <InfoWindow
+                visible={show}
+              >
+                <p className=''>{item.venueName}</p>
+              </InfoWindow>
+            </Marker>
+
+        ))}
+        </Map>
+        {show ? (
+          <button type='button' className='btn-hide' onClick={this.handleBtnHide}>Hide</button>
+        ) : (
+          <button type='button' className='btn-show' onClick={this.handleBtnHide}>Show Map</button>
+        )
+        }
+      </>
+    );
+  };
+};
 
 export default GoogleApiWrapper({
   apiKey: 'AIzaSyCmjvkXB_DMnBUNwxQztLMStyQmA_szbNw'
