746598fc0b647921932cabd52cd59797ee585c41
Samuel Gonzalez <xame2005@gmail.com>
2020-11-03 00:12:54 -0600

    [CREATED] Location of Platzi Bogotá Headquarter on MapComponent.jsx

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
+const markers = [
+  {
+    name: 'Platzi CDMX',
+    lat: 19.4267261,
+    lng: -99.1718706,
+    id: 1,
+  },
+  {
+    name: 'Platzi Bogotá',
+    lat: 4.6560716,
+    lng: -74.0595918,
+    id: 2,
+  },
+];
+
 class MapContainer extends Component {
   constructor(props) {
     super(props);
@@ -29,7 +44,12 @@ class MapContainer extends Component {
             zoom={5}
             initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
           >
-            <Marker position={{ lat: 19.4267261, lng: -99.1718706 }} />
+            {markers.map(marker => (
+              <Marker
+                position={{ lat: marker.lat, lng: marker.lng }}
+                key={marker.id}
+              />
+            ))}
           </Map>
         )}
       </>
